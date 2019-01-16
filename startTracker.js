4c7063accc1c7136843a1a06668b20beb0f2b000
Pablo Biedma <43525988+pablobiedma@users.noreply.github.com>
2019-01-16 14:47:09 +0100

    Delete startTracker.js

--- startTracker.js
+++
@@ -1,7 +0,0 @@
-var gameStatus = {
-    since : Date.now(),     /* since we keep it simple and in-memory, keep track of when this object was created */
-    gamesInitialized : 0,   /* number of games initialized */
-    gamesAborted : 0,       /* number of games aborted */
-    gamesCompleted : 0      /* number of games successfully completed */
-};
-module.exports = gameStatus;
